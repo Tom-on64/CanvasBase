2fd2b9613cc3d378c0715cb53a96e8ef5ff7ae48
Tom-on <87417515+Tom-on64@users.noreply.github.com>
2023-08-25 10:43:03 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Gets called once at start
+ */
+export const start = () => {}
+
+/**
+ * Gets called every frame
+ */
+export const update = () => {}
+
+/**
+ * Gets called at a constant interval (50ms by default)
+ */
+export const fixedUpdate = () => {}
